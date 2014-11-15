8b5e4c112aa68e77fc04a4bb8c0f300e6fb3c36a
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2014-11-15 11:46:31 -0600

    initial attempt to do same thing with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+// To use this, first run these commands:
+// npm install -g grunt-cli
+// npm install grunt --save-dev
+// npm install grunt-contrib-clean --save-dev
+// npm install grunt-traceur-simple --save-dev
+module.exports = function (grunt) {
+  grunt.initConfig({
+    clean: ['dist'],
+    traceur: {
+      options: {
+        // This option includes runtime code in the generated file.
+        //includeRuntime: true,
+        traceurOptions: '--experimental --sourcemap'
+      },
+      all: {
+        files: {
+          // Just need to transpile main file if it imports others.
+          'dist/demo.js': ['src/demo.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-traceur-simple');
+  grunt.registerTask('default', ['traceur']);
+};
