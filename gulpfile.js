4024b3ca534296f7c426759c48cea4bcd84de019
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2014-11-15 11:24:03 -0600

    added clean task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp');
+var rimraf = require('gulp-rimraf');
 var sourcemaps = require('gulp-sourcemaps');
 var traceur = require('gulp-traceur');
 //var concat = require('gulp-concat');
+
+gulp.task('clean', function () {
+  return gulp.src('dist', {read: false})
+    .pipe(rimraf());
+});
 
 gulp.task('default', function () {
   return gulp.src('src/*.js')
